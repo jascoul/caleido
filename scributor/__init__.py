c60c2f15d2424741407b66973aac2a92c74399ee
Jasper Op de Coul <jasper@artudis.com>
2017-09-21 13:22:44 +0200

    Added some swagger views and integration

--- scributor/__init__.py
+++ scributor/__init__.py
@@ -5,6 +5,7 @@ from pyramid.config import Configurator
 def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include("cornice")
+    config.include("cornice_swagger")
     config.include("scributor.storage")
     config.scan("scributor.views")
     return config.make_wsgi_app()
--- scributor/views.py
+++ scributor/views.py
@@ -1,13 +1,76 @@
-""" Cornice services.
-"""
+import colander
 from cornice import Service
+from cornice.service import get_services
+from cornice.validators import colander_body_validator
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from cornice_swagger import CorniceSwagger
 
+
+_VALUES = {}
+
+
+# Create a simple service that will store and retrieve values
+values = Service(name='foo',
+                 path='/values/{key}',
+                 description="Cornice Demo")
+
+
+# Create a body schema for our requests
+class BodySchema(colander.MappingSchema):
+        value = colander.SchemaNode(colander.String(),
+                                    description='My precious value')
+
+
+# Create a response schema for our 200 responses
+class OkResponseSchema(colander.MappingSchema):
+    body = BodySchema()
+    
+
+# Aggregate the response schemas for get requests
+response_schemas = {
+    '200': OkResponseSchema(description='Return value')
+    }
+
+
+# Create our cornice service views
+class MyValueApi(object):
+    """My precious API."""
+    
+    @values.get(tags=['values'], response_schemas=response_schemas)
+    def get_value(request):
+        """Returns the value."""
+        key = request.matchdict['key']
+        return _VALUES.get(key)
+    
+    @values.put(tags=['values'], validators=(colander_body_validator, ),
+                schema=BodySchema(), response_schemas=response_schemas)
+    def set_value(request):
+        """Set the value and returns *True* or *False*."""
+        
+        key = request.matchdict['key']
+        _VALUES[key] = request.json_body
+        return _VALUES.get(key)
+    
+    
+# Create a service to serve our OpenAPI spec
+swagger = Service(name='OpenAPI',
+                  path='__api__',
+                  description="OpenAPI documentation")
+
+
+@swagger.get()
+def openAPI_v1_spec(request):
+    doc = CorniceSwagger(get_services())
+    my_spec = doc.generate('MyAPI', '1.0.0')
+    return my_spec
 
 hello = Service(name='hello', path='/', description="Simplest app")
-
 
 @hello.get()
 def get_info(request):
     """Returns Hello in JSON."""
     return {'Hello': 'World'}
+
+
 
