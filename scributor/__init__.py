6c61ac80379a6d41775ea682a5b28818a70c450e
Jasper Op de Coul <jasper@artudis.com>
2017-09-21 12:09:09 +0200

    Setup for SQLalchemy initialization

--- scributor/__init__.py
+++ scributor/__init__.py
@@ -2,10 +2,11 @@
 """
 from pyramid.config import Configurator
 
-
 def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include("cornice")
+    config.include("pyramid_tm")
+    config.include("scributor.models")
     config.scan("scributor.views")
     return config.make_wsgi_app()
 
