ab90f94e1e14c100944ea265f086b3d29fbeefdb
Jasper Op de Coul <jasper@artudis.com>
2017-09-21 13:22:09 +0200

    Simplified the BaseTest class

--- tests/test_user.py
+++ tests/test_user.py
@@ -11,12 +11,11 @@ def dummy_request(dbsession):
 
 class BaseTest(unittest.TestCase):
     
-    def app_settings(self, **settings):
-        app_settings = {
-            'sqlalchemy.url': 'postgresql://scributor:scr1but0r@localhost/scributor'
+    def app_settings(self):
+        return {
+            'sqlalchemy.url': (
+                'postgresql://scributor:scr1but0r@localhost/scributor')
         }
-        app_settings.update(settings)
-        return app_settings
         
     def setUp(self):
         self.app = main({}, **self.app_settings())
@@ -30,9 +29,11 @@ class BaseTest(unittest.TestCase):
         testing.tearDown()
         transaction.abort()
         self.storage.drop_all()
+
         
 class ScributorTest(BaseTest):
 
-    def test_case(self):
-        self.api.get('/status', status=404)
+    def test_home(self):
+        out = self.api.get('/')
+        assert out.status_code == 200
         
