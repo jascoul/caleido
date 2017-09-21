e1c9e9a1bba03109a1ccd65a9b69f903875852e6
Jasper Op de Coul <jasper@artudis.com>
2017-09-21 14:39:04 +0200

    Factored out base test

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,37 +1,6 @@
-import unittest
-
-import transaction
-from pyramid import testing
-from webtest import TestApp as WebTestApp
-
-from scributor import main
-
-def dummy_request(dbsession):
-    return testing.DummyRequest(dbsession=dbsession)
-
-class BaseTest(unittest.TestCase):
-    
-    def app_settings(self):
-        return {
-            'sqlalchemy.url': (
-                'postgresql://scributor:scr1but0r@localhost/scributor')
-        }
+from core import BaseTest
         
-    def setUp(self):
-        self.app = main({}, **self.app_settings())
-        self.storage = self.app.registry['storage']
-        self.api = WebTestApp(self.app)
-    
-    def init_database(self):
-        self.storage.create_all()
-        
-    def tearDown(self):
-        testing.tearDown()
-        transaction.abort()
-        self.storage.drop_all()
-
-        
-class ScributorTest(BaseTest):
+class UserTest(BaseTest):
 
     def test_home(self):
         out = self.api.get('/')
