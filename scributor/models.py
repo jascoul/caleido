21bd233c9842956d48c7c40d0b164d8819bdff26
Jasper Op de Coul <jasper@artudis.com>
2017-09-21 12:08:03 +0200

    Added sqlalchemy datamodel classes from older work

---
+++ scributor/models.py
@@ -0,0 +1,243 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    Unicode,
+    UnicodeText,
+    Date,
+    Sequence,
+    ForeignKey,
+    ForeignKeyConstraint
+    )
+from sqlalchemy import engine_from_config
+from sqlalchemy.orm import relationship, sessionmaker, configure_mappers
+from sqlalchemy.ext.orderinglist import ordering_list
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import DateRangeType, LtreeType
+import zope.sqlalchemy
+
+Base = declarative_base()
+
+class Kind(Base):
+    __tablename__ = 'kind_schemes'
+    key = Column(Unicode(32), primary_key=True)
+    label = Column(Unicode(128))
+
+    
+class WorkType(Base):
+    __tablename__ = 'work_type_schemes'
+    key = Column(Unicode(32), primary_key=True)
+    kind = Column(Unicode(32),
+                  ForeignKey('kind_schemes.key'),
+                  nullable=False,
+                  primary_key=True)
+    label = Column(Unicode(128))
+
+
+class ActorType(Base):
+    __tablename__ = 'actor_type_schemes'
+    key = Column(Unicode(32), primary_key=True)
+    label = Column(Unicode(128))
+
+
+class UserGroup(Base):
+    __tablename__ = 'user_groups'
+    id = Column(Integer(), primary_key=True)
+    label = Column(Unicode(128))
+
+
+class AccountType(Base):
+    __tablename__ = 'account_type_schemes'
+    key = Column(Unicode(32), primary_key=True)
+    label = Column(Unicode(128))
+
+
+class ContributorRole(Base):
+    __tablename__ = 'contributor_role_schemes'
+    key = Column(Unicode(32), primary_key=True)
+    kind = Column(Unicode(32),
+                  ForeignKey('kind_schemes.key'),
+                  nullable=False,
+                  primary_key=True)
+    label = Column(Unicode(128))
+
+
+class Work(Base):
+    __tablename__ = 'works'
+    __table_args__ = (
+        ForeignKeyConstraint(['type', 'kind'],
+                             ['work_type_schemes.key',
+                              'work_type_schemes.kind']),
+        )
+    id = Column(Integer, Sequence('works_id_seq'), primary_key=True)
+    kind = Column(Unicode(32),
+                  ForeignKey('kind_schemes.key'),
+                  nullable=False)
+    type = Column(Unicode(32),
+                  nullable=False)
+    title = Column(UnicodeText, nullable=False)
+    date = Column(Date, nullable=False)
+    during = Column(DateRangeType, nullable=False)
+    contributors = relationship('Contributor',
+                                back_populates='work',
+                                order_by='Contributor.position',
+                                collection_class=ordering_list('position'))
+
+
+
+class Actor(Base):
+    __tablename__ = 'actors'
+    id = Column(Integer, Sequence('works_id_seq'), primary_key=True)
+    type = Column(Unicode(32),
+                  ForeignKey('actor_type_schemes.key'),
+                  nullable=False)
+    name = Column(Unicode(128), nullable=False)
+    international_name = Column(Unicode(128))
+    native_name = Column(Unicode(128))
+    abbreviated_name = Column(Unicode(64))
+    family_name = Column(Unicode(64))
+    given_name = Column(Unicode(64))
+    initials = Column(Unicode(32))
+    family_name_prefix = Column(Unicode(64))
+    family_name_suffix = Column(Unicode(64))
+
+    memberships = relationship('Membership', back_populates='actor')
+    accounts = relationship('Account', back_populates='actor')
+
+
+class Account(Base):
+    __tablename__ = 'accounts'
+
+    id = Column(Integer, Sequence('accounts_id_seq'), primary_key=True)
+    actor_id = Column(Integer,
+                      ForeignKey('actors.id'),
+                      index=True,
+                      nullable=False)
+    actor = relationship('Actor', back_populates='accounts')
+    type = Column(Unicode(32),
+                  ForeignKey('account_type_schemes.key'),
+                  nullable=False)
+    value = Column(Unicode(128), nullable=False)
+
+    
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, Sequence('users_id_seq'), primary_key=True)
+    user_group = Column(Unicode(32),
+                        ForeignKey('user_groups.id'),
+                        nullable=False)
+    principal = Column(Unicode(128), index=True, nullable=False)
+    credential = Column(Unicode(128), nullable=False)
+
+    
+class Owner(Base):
+    __tablename__ = 'owners'
+    id = Column(Integer, Sequence('owners_id_seq'), primary_key=True)
+    actor_id = Column(Integer,
+                      ForeignKey('actors.id'),
+                      index=True,
+                      nullable=False)
+    principal = Column(Unicode(128), index=True, nullable=False)
+
+    
+class Group(Base):
+    __tablename__ = 'groups'
+    id = Column(Integer, ForeignKey('actors.id'), primary_key=True)
+    path = Column(LtreeType, nullable=False)
+
+
+class Membership(Base):
+    __tablename__ = 'memberships'
+    id = Column(Integer, Sequence('memberships_id_seq'), primary_key=True)
+    actor_id = Column(Integer, ForeignKey('actors.id'), index=True, nullable=False)
+    actor = relationship('Actor', back_populates='memberships')
+    
+    group_id = Column(Integer, ForeignKey('groups.id'), index=True, nullable=False)
+    during = Column(DateRangeType)
+
+    
+class Contributor(Base):
+    "An Actor that made a specific contribution to a work"
+    __tablename__ = 'contributors'
+    __table_args__ = (
+        ForeignKeyConstraint(['role', 'kind'],
+                             ['contributor_role_schemes.key',
+                              'contributor_role_schemes.kind']),
+        )
+    id = Column(Integer, Sequence('contributors_id_seq'), primary_key=True)
+    kind = Column(Unicode(32),
+                  ForeignKey('kind_schemes.key'),
+                  nullable=False)
+    role = Column(Unicode(32),
+                  nullable=False)
+    during = Column(DateRangeType, nullable=True)
+    work_id = Column(Integer, ForeignKey('works.id'), index=True, nullable=False)
+    work = relationship('Work', back_populates='contributors')
+    
+    actor_id = Column(Integer, ForeignKey('actors.id'), index=True, nullable=False)
+    position = Column(Integer)
+
+configure_mappers()
+
+def get_engine(settings, prefix='sqlalchemy.'):
+    return engine_from_config(settings, prefix)
+
+
+def get_session_factory(engine):
+    factory = sessionmaker()
+    factory.configure(bind=engine)
+    return factory
+
+
+def get_tm_session(session_factory, transaction_manager):
+    """
+    Get a ``sqlalchemy.orm.Session`` instance backed by a transaction.
+
+    This function will hook the session to the transaction manager which
+    will take care of committing any changes.
+
+    - When using pyramid_tm it will automatically be committed or aborted
+      depending on whether an exception is raised.
+
+    - When using scripts you should wrap the session in a manager yourself.
+      For example::
+
+          import transaction
+
+          engine = get_engine(settings)
+          session_factory = get_session_factory(engine)
+          with transaction.manager:
+              dbsession = get_tm_session(session_factory, transaction.manager)
+
+    """
+    dbsession = session_factory()
+    zope.sqlalchemy.register(
+        dbsession, transaction_manager=transaction_manager)
+    return dbsession
+
+
+def includeme(config):
+    """
+    Initialize the model for a Pyramid app.
+
+    Activate this setup using ``config.include('scributor.models')``.
+
+    """
+    settings = config.get_settings()
+    settings['tm.manager_hook'] = 'pyramid_tm.explicit_manager'
+
+    # use pyramid_tm to hook the transaction lifecycle to the request
+    config.include('pyramid_tm')
+
+    # use pyramid_retry to retry a request when transient exceptions occur
+    config.include('pyramid_retry')
+    
+    session_factory = get_session_factory(get_engine(settings))
+    config.registry['dbsession_factory'] = session_factory
+
+    # make request.dbsession available for use in Pyramid
+    config.add_request_method(
+        # r.tm is the transaction manager used by pyramid_tm
+        lambda r: get_tm_session(session_factory, r.tm),
+        'dbsession',
+        reify=True
+    )
