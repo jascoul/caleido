3c7e9c361b339fd85e106ab989eeb72c0112895d
Jasper Op de Coul <jasper.opdecoul@eur.nl>
2017-10-12 11:49:56 +0200

    start of actor API

---
+++ caleido/views/actor.py
@@ -0,0 +1,126 @@
+import colander
+from cornice.resource import resource, view
+from cornice.validators import colander_body_validator, colander_validator
+
+from caleido.models import Actor
+from caleido.resources import ResourceFactory, ActorResource
+
+from caleido.utils import (ErrorResponseSchema,
+                           StatusResponseSchema)
+
+class ActorSchema(colander.MappingSchema):
+    id = colander.SchemaNode(colander.Int(), missing=colander.drop)
+    type = colander.SchemaNode(colander.String())
+    name = colander.SchemaNode(colander.String())
+
+class ActorResponseSchema(colander.MappingSchema):
+    body = ActorSchema()
+
+class ActorListingResponseSchema(colander.MappingSchema):
+    @colander.instantiate()
+    class body(colander.MappingSchema):
+        @colander.instantiate()
+        class records(colander.SequenceSchema):
+            actor = ActorSchema()
+        total = colander.SchemaNode(colander.Int())
+        offset = colander.SchemaNode(colander.Int())
+        limit = colander.SchemaNode(colander.Int())
+
+class ActorListingRequestSchema(colander.MappingSchema):
+    @colander.instantiate()
+    class querystring(colander.MappingSchema):
+        offset = colander.SchemaNode(colander.Int(),
+                                   default=0,
+                                   validator=colander.Range(min=0),
+                                   missing=0)
+        limit = colander.SchemaNode(colander.Int(),
+                                    default=20,
+                                    validator=colander.Range(0, 100),
+                                    missing=20)
+
+@resource(name='Actor',
+          collection_path='/api/v1/actors',
+          path='/api/v1/actors/{id}',
+          tags=['actor'],
+          api_security=[{'jwt':[]}],
+          factory=ResourceFactory(ActorResource))
+class ActorAPI(object):
+    def __init__(self, request, context):
+        self.request = request
+        self.context = context
+
+    @view(permission='view',
+          response_schemas={
+        '200': ActorResponseSchema(description='Ok'),
+        '401': ErrorResponseSchema(description='Unauthorized'),
+        '403': ErrorResponseSchema(description='Forbidden'),
+        '404': ErrorResponseSchema(description='Not Found'),
+        })
+    def get(self):
+        "Retrieve a Actor"
+        return ActorSchema().serialize(self.context.model.to_dict())
+
+    @view(permission='edit',
+          schema=ActorSchema(),
+          validators=(colander_body_validator,),
+          response_schemas={
+        '200': ActorResponseSchema(description='Ok'),
+        '401': ErrorResponseSchema(description='Unauthorized'),
+        '403': ErrorResponseSchema(description='Forbidden'),
+        '404': ErrorResponseSchema(description='Not Found'),
+        })
+    def put(self):
+        "Modify an Actor"
+        self.context.model.update_dict(self.request.validated)
+        self.context.put()
+        return ActorSchema().serialize(self.context.model.to_dict())
+
+
+    @view(permission='delete',
+          response_schemas={
+        '200': StatusResponseSchema(description='Ok'),
+        '401': ErrorResponseSchema(description='Unauthorized'),
+        '403': ErrorResponseSchema(description='Forbidden'),
+        '404': ErrorResponseSchema(description='Not Found'),
+        })
+    def delete(self):
+        "Delete an Actor"
+        self.context.delete()
+        return {'status': 'ok'}
+
+    @view(permission='add',
+          schema=ActorSchema(),
+          validators=(colander_body_validator,),
+          response_schemas={
+        '201': ActorResponseSchema(description='Created'),
+        '400': ErrorResponseSchema(description='Bad Request'),
+        '401': ErrorResponseSchema(description='Unauthorized'),
+        '403': ErrorResponseSchema(description='Forbidden'),
+        })
+    def collection_post(self):
+        "Create a new Actor"
+        actor = Actor.from_dict(self.request.validated)
+        self.context.put(actor)
+        self.request.response.status = 201
+        return ActorSchema().serialize(actor.to_dict())
+
+
+    @view(permission='view',
+          schema=ActorListingRequestSchema(),
+          validators=(colander_validator),
+          response_schemas={
+        '200': ActorListingResponseSchema(description='Ok'),
+        '400': ErrorResponseSchema(description='Bad Request'),
+        '401': ErrorResponseSchema(description='Unauthorized')})
+    def collection_get(self):
+        offset = self.request.validated['querystring']['offset']
+        limit = self.request.validated['querystring']['limit']
+        listing = self.context.search(
+            offset=offset,
+            limit=limit,
+            principals=self.request.effective_principals)
+        return {'total': listing['total'],
+                'records': [actor.to_dict() for actor in listing['hits']],
+                'limit': limit,
+                'offset': offset}
+
---
+++ tests/test_actor.py
@@ -0,0 +1,37 @@
+
+from core import BaseTest
+
+class ActorWebTest(BaseTest):
+
+    def test_crud_as_admin(self):
+        headers = dict(Authorization='Bearer %s' % self.admin_token())
+        out = self.api.post_json('/api/v1/actors',
+                                 {'name': 'John', 'type': 'individual'},
+                                 headers=headers,
+                                 status=201)
+        john_id = out.json['id']
+        # Change Johns name to Johnny
+        self.api.put_json('/api/v1/actors/%s' % john_id,
+                          {'name': 'Johnny', 'type': 'individual'},
+                          headers=headers,
+                          status=200)
+        out = self.api.get('/api/v1/actors/%s' % john_id,
+                          headers=headers,
+                          status=200)
+        assert out.json['name'] == 'Johnny'
+        self.api.delete('/api/v1/actors/%s' % john_id,
+                        headers=headers,
+                        status=200)
+        self.api.get('/api/v1/actors/%s' % john_id,
+                          headers=headers,
+                          status=404)
+
+    def test_invalid_actor_type(self):
+        headers = dict(Authorization='Bearer %s' % self.admin_token())
+        out = self.api.post_json('/api/v1/actors',
+                                 {'name': 'John', 'type': 'foobar'},
+                                 headers=headers,
+                                 status=400)
+        import pdb;pdb.set_trace()
+
+
